198872dd0e09cdf8839999062c1a4095e09d3008
A-Pogam <alicepogam59@hotmail.com>
2024-07-10 16:32:44 +0200

    improved user

--- src/main/java/com/openclassrooms/tourguide/model/User.java
+++ src/main/java/com/openclassrooms/tourguide/model/User.java
@@ -1,4 +1,4 @@
-package com.openclassrooms.tourguide.user;
+package com.openclassrooms.tourguide.model;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -9,94 +9,96 @@ import gpsUtil.location.VisitedLocation;
 import tripPricer.Provider;
 
 public class User {
+
 	private final UUID userId;
 	private final String userName;
 	private String phoneNumber;
 	private String emailAddress;
 	private Date latestLocationTimestamp;
+
 	private List<VisitedLocation> visitedLocations = new ArrayList<>();
-	private List<UserReward> userRewards = new ArrayList<>();
-	private UserPreferences userPreferences = new UserPreferences();
+	private List<Reward> rewards = new ArrayList<>();
+	private Preferences preferences = new Preferences();
 	private List<Provider> tripDeals = new ArrayList<>();
+
 	public User(UUID userId, String userName, String phoneNumber, String emailAddress) {
 		this.userId = userId;
 		this.userName = userName;
 		this.phoneNumber = phoneNumber;
 		this.emailAddress = emailAddress;
 	}
-	
+
 	public UUID getUserId() {
 		return userId;
 	}
-	
+
 	public String getUserName() {
 		return userName;
 	}
-	
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
 	public void setPhoneNumber(String phoneNumber) {
 		this.phoneNumber = phoneNumber;
 	}
-	
-	public String getPhoneNumber() {
-		return phoneNumber;
+
+	public String getEmailAddress() {
+		return emailAddress;
 	}
 
 	public void setEmailAddress(String emailAddress) {
 		this.emailAddress = emailAddress;
 	}
-	
-	public String getEmailAddress() {
-		return emailAddress;
-	}
-	
-	public void setLatestLocationTimestamp(Date latestLocationTimestamp) {
-		this.latestLocationTimestamp = latestLocationTimestamp;
-	}
-	
+
 	public Date getLatestLocationTimestamp() {
 		return latestLocationTimestamp;
 	}
-	
-	public void addToVisitedLocations(VisitedLocation visitedLocation) {
-		visitedLocations.add(visitedLocation);
+
+	public void setLatestLocationTimestamp(Date latestLocationTimestamp) {
+		this.latestLocationTimestamp = latestLocationTimestamp;
 	}
-	
+
 	public List<VisitedLocation> getVisitedLocations() {
 		return visitedLocations;
 	}
-	
+
+	public void addToVisitedLocations(VisitedLocation visitedLocation) {
+		visitedLocations.add(visitedLocation);
+	}
+
 	public void clearVisitedLocations() {
 		visitedLocations.clear();
 	}
-	
-	public void addUserReward(UserReward userReward) {
-		if(userRewards.stream().filter(r -> !r.attraction.attractionName.equals(userReward.attraction)).count() == 0) {
-			userRewards.add(userReward);
-		}
+
+	public List<Reward> getRewards() {
+		return rewards;
 	}
-	
-	public List<UserReward> getUserRewards() {
-		return userRewards;
+
+	public Preferences getPreferences() {
+		return preferences;
 	}
-	
-	public UserPreferences getUserPreferences() {
-		return userPreferences;
+
+	public void setPreferences(Preferences preferences) {
+		this.preferences = preferences;
 	}
-	
-	public void setUserPreferences(UserPreferences userPreferences) {
-		this.userPreferences = userPreferences;
+
+	public List<Provider> getTripDeals() {
+		return tripDeals;
+	}
+
+	public void setTripDeals(List<Provider> tripDeals) {
+		this.tripDeals = tripDeals;
 	}
 
 	public VisitedLocation getLastVisitedLocation() {
 		return visitedLocations.get(visitedLocations.size() - 1);
 	}
-	
-	public void setTripDeals(List<Provider> tripDeals) {
-		this.tripDeals = tripDeals;
-	}
-	
-	public List<Provider> getTripDeals() {
-		return tripDeals;
-	}
 
+	public void addReward(Reward reward) {
+		if (rewards.stream().filter(r -> r.attraction.attractionName.equals(reward.attraction.attractionName)).count() == 0) {
+			rewards.add(reward);
+		}
+	}
 }
