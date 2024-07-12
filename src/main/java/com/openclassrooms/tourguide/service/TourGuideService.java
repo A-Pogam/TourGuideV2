7b8913d65e962abd2a731e8077609d8cb5be8669
A-Pogam <alicepogam59@hotmail.com>
2024-07-12 16:35:41 +0200

    Improved TourguideService

--- src/main/java/com/openclassrooms/tourguide/service/TourGuideService.java
+++ src/main/java/com/openclassrooms/tourguide/service/TourGuideService.java
@@ -1,109 +1,160 @@
 package com.openclassrooms.tourguide.service;
 
-import com.openclassrooms.tourguide.helper.InternalTestHelper;
-import com.openclassrooms.tourguide.tracker.Tracker;
-import com.openclassrooms.tourguide.user.User;
-import com.openclassrooms.tourguide.user.UserReward;
-
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Locale;
-import java.util.Map;
-import java.util.Random;
-import java.util.UUID;
+import java.util.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
+import com.openclassrooms.tourguide.dto.AttractionNearUserDto;
+import com.openclassrooms.tourguide.dto.NearbyAttractionDto;
+import com.openclassrooms.tourguide.dto.UserLocationDto;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.config.Task;
 import org.springframework.stereotype.Service;
+
+import com.openclassrooms.tourguide.constant.ApiKey;
+import com.openclassrooms.tourguide.model.User;
+import com.openclassrooms.tourguide.model.Reward;
+import com.openclassrooms.tourguide.service.contracts.IRewardService;
+import com.openclassrooms.tourguide.service.contracts.ITourGuideService;
+import com.openclassrooms.tourguide.util.InternalUsersInitializer;
+import com.openclassrooms.tourguide.util.Tracker;
 
 import gpsUtil.GpsUtil;
 import gpsUtil.location.Attraction;
-import gpsUtil.location.Location;
 import gpsUtil.location.VisitedLocation;
-
+import org.w3c.dom.Attr;
 import tripPricer.Provider;
 import tripPricer.TripPricer;
 
 @Service
-public class TourGuideService {
-	private Logger logger = LoggerFactory.getLogger(TourGuideService.class);
-	private final GpsUtil gpsUtil;
-	private final RewardsService rewardsService;
-	private final TripPricer tripPricer = new TripPricer();
-	public final Tracker tracker;
-	boolean testMode = true;
+public class TourGuideService implements ITourGuideService {
 
-	public TourGuideService(GpsUtil gpsUtil, RewardsService rewardsService) {
+	@Autowired
+	private IRewardService iRewardService;
+
+	private Logger logger = LoggerFactory.getLogger(TourGuideService.class);
+
+	private final GpsUtil gpsUtil;
+	private final TripPricer tripPricer = new TripPricer();
+	private final InternalUsersInitializer internalUsers = new InternalUsersInitializer();
+
+	private Map<String, User> internalUserMap;
+	private boolean testMode = true;
+
+	public final Tracker tracker;
+
+	public TourGuideService(GpsUtil gpsUtil, IRewardService iRewardService) {
 		this.gpsUtil = gpsUtil;
-		this.rewardsService = rewardsService;
-		
+		this.iRewardService = iRewardService;
+
 		Locale.setDefault(Locale.US);
 
 		if (testMode) {
 			logger.info("TestMode enabled");
 			logger.debug("Initializing users");
-			initializeInternalUsers();
+			internalUserMap = internalUsers.initializeInternalUsers();
 			logger.debug("Finished initializing users");
 		}
 		tracker = new Tracker(this);
 		addShutDownHook();
 	}
-
-	public List<UserReward> getUserRewards(User user) {
-		return user.getUserRewards();
+	
+	@Override
+	public Tracker getTracker() {
+		return tracker;
+	}
+	
+	@Override
+	public List<User> getAllUsers() {
+		return internalUserMap.values().stream().collect(Collectors.toList());
 	}
 
+	@Override
+	public User getUser(String userName) {
+		return internalUserMap.get(userName);
+	}
+
+	@Override
 	public VisitedLocation getUserLocation(User user) {
 		VisitedLocation visitedLocation = (user.getVisitedLocations().size() > 0) ? user.getLastVisitedLocation()
 				: trackUserLocation(user);
 		return visitedLocation;
 	}
 
-	public User getUser(String userName) {
-		return internalUserMap.get(userName);
+	@Override
+	public List<Reward> getUserRewards(User user) {
+		return user.getRewards();
 	}
 
-	public List<User> getAllUsers() {
-		return internalUserMap.values().stream().collect(Collectors.toList());
+	@Override
+	public List<Provider> getTripDeals(User user) {
+		int cumulatativeRewardPoints = user.getRewards().stream().mapToInt(i -> i.getRewardPoints()).sum();
+		List<Provider> providers = tripPricer.getPrice(ApiKey.TRIP_PRICER_API_KEY, user.getUserId(),
+				user.getPreferences().getNumberOfAdults(), user.getPreferences().getNumberOfChildren(),
+				user.getPreferences().getTripDuration(), cumulatativeRewardPoints);
+		user.setTripDeals(providers);
+		return providers;
 	}
 
+	@Override
+	public List<Attraction> getNearByAttractions(VisitedLocation visitedLocation) {
+		List<Attraction> nearbyAttractions = new ArrayList<>();
+		TreeMap<Double, Attraction> attractionByDistance = new TreeMap<>();
+		for (Attraction attraction : gpsUtil.getAttractions()) {
+			Double distance = iRewardService.getDistance(visitedLocation.location, attraction);
+			attractionByDistance.put(distance, attraction);
+		}
+		for (Map.Entry<Double, Attraction> entry: attractionByDistance.entrySet()) {
+			nearbyAttractions.add(entry.getValue());
+		}
+		return nearbyAttractions.stream().limit(5).collect(Collectors.toList());
+	}
+
+	@Override
+	public List<AttractionNearUserDto> nearbyAttractionsToDto(VisitedLocation visitedLocation, User user, List<Attraction> nearbyAttractions) {
+		UserLocationDto userLocationDto = new UserLocationDto(visitedLocation.location.longitude, visitedLocation.location.latitude);
+		List<AttractionNearUserDto> formatedNearbyAttractions = new ArrayList<>();
+
+		for (Attraction attraction:nearbyAttractions) {
+			NearbyAttractionDto nearbyAttractionDto = new NearbyAttractionDto(attraction.attractionName, attraction.longitude, attraction.latitude);
+			AttractionNearUserDto attractionNearUserDto = new AttractionNearUserDto(nearbyAttractionDto, userLocationDto, iRewardService.getDistance(visitedLocation.location, attraction), iRewardService.getRewards(attraction, user));
+			formatedNearbyAttractions.add(attractionNearUserDto);
+		}
+		return formatedNearbyAttractions;
+	}
+
+	@Override
 	public void addUser(User user) {
 		if (!internalUserMap.containsKey(user.getUserName())) {
 			internalUserMap.put(user.getUserName(), user);
 		}
 	}
 
-	public List<Provider> getTripDeals(User user) {
-		int cumulatativeRewardPoints = user.getUserRewards().stream().mapToInt(i -> i.getRewardPoints()).sum();
-		List<Provider> providers = tripPricer.getPrice(tripPricerApiKey, user.getUserId(),
-				user.getUserPreferences().getNumberOfAdults(), user.getUserPreferences().getNumberOfChildren(),
-				user.getUserPreferences().getTripDuration(), cumulatativeRewardPoints);
-		user.setTripDeals(providers);
-		return providers;
-	}
-
+	@Override
 	public VisitedLocation trackUserLocation(User user) {
 		VisitedLocation visitedLocation = gpsUtil.getUserLocation(user.getUserId());
 		user.addToVisitedLocations(visitedLocation);
-		rewardsService.calculateRewards(user);
+		iRewardService.calculateRewards(user);
 		return visitedLocation;
 	}
 
-	public List<Attraction> getNearByAttractions(VisitedLocation visitedLocation) {
-		List<Attraction> nearbyAttractions = new ArrayList<>();
-		for (Attraction attraction : gpsUtil.getAttractions()) {
-			if (rewardsService.isWithinAttractionProximity(attraction, visitedLocation.location)) {
-				nearbyAttractions.add(attraction);
-			}
+	@Override
+	public List<VisitedLocation> trackAllUsersLocation(List<User> users) throws Exception {
+		ExecutorService executor = Executors.newFixedThreadPool(100);
+		List<VisitedLocation> usersLocation = new ArrayList<>();
+		for (User user : users) {
+			executor.execute(()->{
+				VisitedLocation visitedLocation = trackUserLocation(user);
+				usersLocation.add(visitedLocation);
+			});
 		}
-
-		return nearbyAttractions;
+		executor.shutdown();
+		executor.awaitTermination(Long.MAX_VALUE, TimeUnit.DAYS);
+		return usersLocation;
 	}
 
 	private void addShutDownHook() {
@@ -114,51 +165,5 @@ public class TourGuideService {
 		});
 	}
 
-	/**********************************************************************************
-	 * 
-	 * Methods Below: For Internal Testing
-	 * 
-	 **********************************************************************************/
-	private static final String tripPricerApiKey = "test-server-api-key";
-	// Database connection will be used for external users, but for testing purposes
-	// internal users are provided and stored in memory
-	private final Map<String, User> internalUserMap = new HashMap<>();
-
-	private void initializeInternalUsers() {
-		IntStream.range(0, InternalTestHelper.getInternalUserNumber()).forEach(i -> {
-			String userName = "internalUser" + i;
-			String phone = "000";
-			String email = userName + "@tourGuide.com";
-			User user = new User(UUID.randomUUID(), userName, phone, email);
-			generateUserLocationHistory(user);
-
-			internalUserMap.put(userName, user);
-		});
-		logger.debug("Created " + InternalTestHelper.getInternalUserNumber() + " internal test users.");
-	}
-
-	private void generateUserLocationHistory(User user) {
-		IntStream.range(0, 3).forEach(i -> {
-			user.addToVisitedLocations(new VisitedLocation(user.getUserId(),
-					new Location(generateRandomLatitude(), generateRandomLongitude()), getRandomTime()));
-		});
-	}
-
-	private double generateRandomLongitude() {
-		double leftLimit = -180;
-		double rightLimit = 180;
-		return leftLimit + new Random().nextDouble() * (rightLimit - leftLimit);
-	}
-
-	private double generateRandomLatitude() {
-		double leftLimit = -85.05112878;
-		double rightLimit = 85.05112878;
-		return leftLimit + new Random().nextDouble() * (rightLimit - leftLimit);
-	}
-
-	private Date getRandomTime() {
-		LocalDateTime localDateTime = LocalDateTime.now().minusDays(new Random().nextInt(30));
-		return Date.from(localDateTime.toInstant(ZoneOffset.UTC));
-	}
 
 }
