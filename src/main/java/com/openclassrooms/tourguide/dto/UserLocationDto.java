119cae57ac9575d0e67ab45abb4b8d70a23568c1
A-Pogam <alicepogam59@hotmail.com>
2024-07-19 15:58:08 +0200

    Created UserLocationDto

---
+++ src/main/java/com/openclassrooms/tourguide/dto/UserLocationDto.java
@@ -0,0 +1,29 @@
+package com.openclassrooms.tourguide.dto;
+
+public class UserLocationDto {
+    private Double longitude;
+
+    private Double latitude;
+
+    public UserLocationDto(Double longitude, Double latitude) {
+        this.longitude = longitude;
+        this.latitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+}
