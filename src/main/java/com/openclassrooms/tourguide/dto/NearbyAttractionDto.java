3d3eea5bd730df3f9b806c93fd1a6ab2b88e2d57
A-Pogam <alicepogam59@hotmail.com>
2024-07-19 15:25:54 +0200

    Created NearbyAttractionDto

---
+++ src/main/java/com/openclassrooms/tourguide/dto/NearbyAttractionDto.java
@@ -0,0 +1,44 @@
+package com.openclassrooms.tourguide.dto;
+
+public class NearbyAttractionDto {
+
+    private String name;
+
+    private Double longitude;
+
+    private Double latitude;
+
+
+    public NearbyAttractionDto(String name, Double longitude, Double latitude) {
+        this.name = name;
+        this.longitude = longitude;
+        this.latitude = latitude;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+
+
+}
